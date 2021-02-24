bbe3cba697d4b70a9415dba9321d6c57b483d4d8
p.mazunin <p.mazunin@xsolla.com>
2021-02-24 21:22:32 +0500

    add cypress

---
+++ cypress/integration/yandex_weather.spec.js
@@ -0,0 +1,26 @@
+describe('Temperature test', () => {
+    it('Fails if one of the visible daily daytime temperature is NOT negative', () => {
+      cy.visit('https://yandex.ru/')
+//Workaround to avoid opening in new tab
+      cy.get('.weather__header > h1 > .home-link').then(function ($e) {
+        const href = $e.prop('href')
+        cy.visit(href)
+        cy.url()
+        .should('contains', 'pogoda')
+      })
+//Searh input. I can specify 'location' in cypress.json
+      cy.get('.mini-suggest-form')
+      .type(Cypress.env('location'))
+//1st popup suggestion
+      cy.xpath('//div[contains(@class,"popup")]/ul/li[1]')
+      .click()
+      
+//Visible daily daytime temperatures
+      cy.xpath('//div[contains(@class,"temp_day")]/span[2]')
+      .filter(':visible')
+      .each(($el, index, $list) => {
+        cy.wrap($el).invoke('text')
+        .should('contains', '−')
+    })
+  })
+})
